2f0cf843f618f2660efa1db9c1ea4d038e419682
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-02-24 23:10:08 -0800

    [Refactor] Add Calculator Model

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,24 +10,47 @@ import Foundation
 struct CalculatorLogic {
     
     private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
 
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
-            if symbol == "+/-" {
+            switch symbol {
+            case "+/-":
                 return n * -1
-            }
-            if symbol == "AC" {
+            case "AC":
                 return 0
-            }
-            if symbol == "%" {
+            case "%":
                 return n / 100
+            case "=":
+                return performTwoCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
         return nil
     }
-
+    
+    private func performTwoCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation doesn't match.")
+            }
+        }
+        return nil
+    }
+    
 }
